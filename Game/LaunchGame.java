3ba218714f2e26188ab7e4b5a68f57c95a0fdaf0
Aayush-spec-alt <aayush02122001@gmail.com>
2025-05-10 01:51:16 +0530

    Create LaunchGame.java

---
+++ Game/LaunchGame.java
@@ -0,0 +1,51 @@
+
+class Player
+{
+
+}
+
+
+class Guesser
+{
+
+}
+
+class Umpire
+{
+    int numGuess;
+    int numP1;
+    int numP2;
+    int numP3;
+
+    public void getNumberFromGuesser()
+    {
+        
+    }
+    public void collectFromPlayers()
+    {
+
+    }
+    public void calcResult()
+    {
+
+    }
+    
+}
+
+
+public class LaunchGame {
+
+
+    public static void main(String[] args) {
+        Umpire umpire = new Input();
+        umpire.getNumberFromGuesser();
+        umpire.collectFromPlayers();
+        umpire.calcResult();
+
+
+
+    }
+
+
+
+}
